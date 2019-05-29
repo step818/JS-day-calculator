f0327256e8017640b9de0b994966bb965e292830
Guest User <Guest@epicodus-79.local>
2019-05-29 09:13:42 -0700

    blank file temp

---
+++ spec/name-me-spec.js
@@ -0,0 +1,10 @@
+import { functionName } from "../src/location";
+
+describe('functionName', function() {
+
+//test example:
+    // it('should test whether a number is modular of three', function() {
+    // var goal = new pingPong(3);
+    // expect(goal).toEqual([1,2,"ping"]);
+    });
+});
